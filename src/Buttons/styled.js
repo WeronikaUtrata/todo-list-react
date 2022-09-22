9cccd723401c7ac566385daeff34c5ff61db36de
Weronika Utrata <werutr@wp.pl>
2022-09-22 16:27:32 +0200

    styled js added to buttons component

---
+++ src/Buttons/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+`;
+
+export const Button = styled.button`
+    background: transparent;
+    color: teal;
+    border: none;
+    transition: 0.5s;
+
+    @media (max-width: 767px) {
+        flex-basis: 100%;
+        margin: 10px;
+}
+
+    &:hover {
+        color: rgb(19, 180, 180);
+        cursor: pointer;
+    }
+
+    &:disabled {
+        color: #ccc;
+        cursor: not-allowed;
+    }
+`;
